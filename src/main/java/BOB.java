df9d87774394a04770008b8ab95f913b84b1027e
xyanjun02 <yanjunxie611@gmail.com>
2025-01-24 11:08:02 +0800

    level 0

---
+++ src/main/java/BOB.java
@@ -0,0 +1,8 @@
+public class BOB {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm BOB");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+}
