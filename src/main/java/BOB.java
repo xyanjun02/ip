5617ffea13bc400acee02b95a77e01382eeb006a
xyanjun02 <yanjunxie611@gmail.com>
2025-02-03 14:38:53 +0800

    level -1

--- src/main/java/BOB.java
+++ src/main/java/BOB.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class BOB {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm BOB");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye! Hope to see you again soon!\uD83D\uDE00");
+                System.out.println(" - BOB");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
